678501caf0341de0109167cd7d57a1ae3c5f10a0
kylecho <kylecho.work@gmail.com>
2015-08-28 20:22:14 -0700

    Add scoreboard, fix collision

--- client/watchout.js
+++ client/watchout.js
@@ -1,5 +1,9 @@
 // start slingin' some d3 here.
 // set initial values
+var highScore = 0;
+var currScore = 0;
+var numCollisions = 0;
+var lastCollision = Date.now();
 var width = 960;
 var height = 500;
 const numEnemies = 50;
@@ -53,5 +57,46 @@ d3.select('svg').selectAll('circle')
 
 // when they first move the thingy, AAAAAH THEY'RE COMING
 setInterval(moveEnemies, 3000);
+
+// run a function every, say, hundredth of a second or whatever
+// check if anything is touching the player
+// if so, do things
+// otherwise, dont do those things
+setInterval(checkCollision, 10);
+
+// we want to change value inside span inside div with class 'high'
+
+function checkCollision() {
+  // compare the player's position with any enemy's position.
+  // instead of using player.x and enemies[i].x
+  // we'll use d3 and use d.cx and whatever else
+  currScore++;
+
+  d3.selectAll('circle').each(function(d, i){
+    // if we're within a small distance, update highscore, curr score, nC
+    var midx = myPlayer.x + .5 * myPlayer.size;
+    var midy = myPlayer.y + .5 * myPlayer.size;
+    
+    var circx = d3.select(this).attr('cx');
+    var circy = d3.select(this).attr('cy');
+    if (Math.sqrt(Math.pow(midx-circx,2)+Math.pow(midy-circy,2)) <= 25) {
+      if (Date.now() - lastCollision > 1000) {
+        numCollisions++;
+        lastCollision = Date.now();
+      }
+      if (currScore > highScore) {
+        highScore = currScore;
+        d3.select('div.high').select('span').data([highScore])
+          .text(highScore);
+      }
+      currScore = 0; 
+    }    
+  });
+
+  d3.select('div.current').select('span').data([currScore])
+      .text(currScore);
+  d3.select('div.collisions').select('span').data([numCollisions])
+      .text(numCollisions);
+}
 
 
